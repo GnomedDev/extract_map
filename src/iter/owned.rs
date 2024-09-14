f4cd45a6dca2112cf7829354e4264f5c6d86bf45
GnomedDev <david2005thomas@gmail.com>
2024-09-14 14:16:08 +0100

    Use HashTable, instead of HashSet

--- src/iter/owned.rs
+++ src/iter/owned.rs
@@ -1,21 +1,15 @@
 use std::iter::FusedIterator;
 
-use crate::{value_wrapper::ValueWrapper, ExtractMap};
+use crate::ExtractMap;
 
 #[must_use = "Iterators do nothing if not consumed"]
-pub struct IntoIter<K, V>(std::collections::hash_set::IntoIter<ValueWrapper<K, V>>);
+pub struct IntoIter<V>(hashbrown::hash_table::IntoIter<V>);
 
-impl<K, V: std::fmt::Debug> std::fmt::Debug for IntoIter<K, V> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Debug::fmt(&self.0, f)
-    }
-}
-
-impl<K, V> Iterator for IntoIter<K, V> {
+impl<V> Iterator for IntoIter<V> {
     type Item = V;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|v| v.0)
+        self.0.next()
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -23,19 +17,19 @@ impl<K, V> Iterator for IntoIter<K, V> {
     }
 }
 
-impl<K, V> ExactSizeIterator for IntoIter<K, V> {
+impl<V> ExactSizeIterator for IntoIter<V> {
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl<K, V> FusedIterator for IntoIter<K, V> {}
+impl<V> FusedIterator for IntoIter<V> {}
 
 impl<K, V, S> IntoIterator for ExtractMap<K, V, S> {
     type Item = V;
-    type IntoIter = IntoIter<K, V>;
+    type IntoIter = IntoIter<V>;
 
     fn into_iter(self) -> Self::IntoIter {
-        IntoIter(self.inner.into_iter())
+        IntoIter(self.table.into_iter())
     }
 }
